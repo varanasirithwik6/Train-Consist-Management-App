73a638ee533f95e33919fe1728b42cecf0d9b380
varanasirithwik6 <rithwikbblvarun@gmail.com>
2026-04-16 16:36:35 +0530

    UC19: Binary Search for Bogie ID (Optimized Searching)

--- TrainConsistManagement/src/TrainConsistManagementApp.java
+++ TrainConsistManagement/src/TrainConsistManagementApp.java
@@ -1,37 +1,41 @@
+import java.util.Arrays;
+
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
 
-        System.out.println("======================================");
-        System.out.println("UC18 - Linear Search for Bogie ID");
-        System.out.println("======================================\n");
+        System.out.println("UC19 - Binary Search for Bogie ID\n");
 
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
-        String searchId = "BG309";
 
-        // Display all bogies
-        System.out.println("Available Bogie IDs:");
-        for (String id : bogieIds) {
-            System.out.println(id);
-        }
+        // Sort first (IMPORTANT)
+        Arrays.sort(bogieIds);
 
-        // Linear Search
+        String key = "BG309";
+
+        int low = 0;
+        int high = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(searchId)) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int result = key.compareTo(bogieIds[mid]);
+
+            if (result == 0) {
                 found = true;
-                break; // stop when found
+                break;
+            } else if (result > 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
-        // Result
         if (found) {
-            System.out.println("\nBogie " + searchId + " found in train consist.");
+            System.out.println("Bogie " + key + " found using Binary Search.");
         } else {
-            System.out.println("\nBogie " + searchId + " NOT found.");
+            System.out.println("Bogie not found.");
         }
-
-        System.out.println("\nUC18 search completed...");
     }
 }
